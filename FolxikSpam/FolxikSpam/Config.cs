5ebc834884994996f8f1ae7881421865e2f04689
folxik <folxgaming2@gmail.com>
2016-02-26 11:49:00 +0100

    Lets go!

---
+++ FolxikSpam/FolxikSpam/Config.cs
@@ -0,0 +1,39 @@
+﻿using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace FolxikSpam
+{
+    public static class Config
+    {
+        private const string MenuName = "FolxikSpam";
+        private static readonly Menu Menu;
+
+        static Config()
+        {
+            Menu = MainMenu.AddMenu(MenuName, MenuName.ToLower());
+            Menu.AddGroupLabel("ENG Version");
+            Menu.AddLabel("Welcome in Addon FolxikSpam!");
+            Menu.AddLabel("This Addon is in the beta version!");
+            Menu.AddLabel("If you found some bug, report him in the subject on the forum EloBuddy.net!");
+            Menu.AddGroupLabel("PL Version");
+            Menu.AddLabel("Witaj w Addonie FolxikSpam!");
+            Menu.AddLabel("Ten Addon jest w wersji beta!");
+            Menu.AddLabel("Jeżeli znalazłeś jakiś bug, zgłoś go w temacie na forum EloBuddy.net");
+        }
+        public static void Initialize()
+        {
+        }
+        public static class Modes
+        {
+            private static readonly Menu Menu;
+            static Modes()
+            {
+                Menu = Config.Menu.AddSubMenu("Modes");
+                Menu.AddSeparator();
+            }
+            public static void Initialize()
+            {
+            }
+        }
+    }
+}
---
+++ FolxikSpam/FolxikSpam/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace FolxikSpam
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            Config.Initialize(); 
+            
+            Chat.Print("<font color='#660033'>FolxikSpam Loaded!</font><font color='#33FF00'>Have fun!");
+            Chat.Print("<font color='##FF9900'>Addon working on patch</font><font color='#EBC79E'>6.4.0.250!");
+            Drawing.OnDraw += OnDraw;
+        }
+        private static void OnDraw(EventArgs args)
+        {
+        }
+    }
+}
